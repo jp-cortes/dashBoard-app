a86efbaf8a27ccd2170d4d8f8dba33524f863523
Juan Cortes <alias99002@outlook.com>
2023-02-07 15:21:20 -0500

    Setup of the components and navigation

---
+++ src/components/index.jsx
@@ -0,0 +1,19 @@
+////pednding
+export { Button } from './Button';
+export { Cart } from './Cart';
+export { Chat } from './Chat'
+export { Footer } from './Footer'
+export { Header } from './Header'
+export { Navbar } from './Navbar'
+export { Notifications } from './Notifications'
+export { Sidebar } from './Sidebar'
+export { ThemeSettings } from './ThemeSettings'
+export { UserProfile } from './UserProfile'
+export { ChartsHeader } from './ChartsHeader'
+
+
+//Charts
+export { LineChart } from './Charts/LineChart';
+export { Pie } from './Charts/Pie';
+export { SparkLine } from './Charts/SparkLine';
+export { Stacked } from './Charts/Stacked';
---
+++ src/pages/index.jsx
@@ -0,0 +1,20 @@
+//pages
+export { Calendar } from './Calendar';
+export { ColorPicker } from './ColorPicker';
+export { Customers } from './Customers';
+export { Ecommerce } from './Ecommerce';
+export { Editor } from './Editor';
+export { Employees } from './Employees';
+export { Kanban } from './Kanban';
+export { Orders } from './Orders';
+
+//charts
+
+export { Area } from './Charts/Area';
+export { Bar } from './Charts/Bar';
+export { ColorMapping } from './Charts/ColorMapping';
+export { Financial } from './Charts/Financial';
+export { Line } from './Charts/Line';
+export { Pie } from './Charts/Pie';
+export { Pyramid } from './Charts/Pyramid';
+export { Stacked } from './Charts/Stacked';
